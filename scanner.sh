b35b7333fa9e902c416797771df291069d77cdf8
sumgr0 <sumit.grover@gmail.com>
2019-11-07 13:05:11 +0530

    fixed: dnsgen output formatting

--- scanner.sh
+++ scanner.sh
@@ -160,8 +160,8 @@ echo "[+] DNSGEN SCANNING [+]"
 if [ ! -f ~/recon/$1/$1-dnsgen.txt ] && [ ! -z $(which dnsgen) ]; then
 	[ ! -f ~/recon/scanner/dnsgen.txt ] && wget "https://raw.githubusercontent.com/infosec-au/altdns/master/words.txt" -O ~/recon/scanner/dnsgen.txt
 	rm ~/recon/$1/$1-dnsgen.txt
-	cat ~/recon/$1/$1-final.txt | dnsgen - | massdns -r ~/tools/massdns/lists/resolvers.txt -t A -o J --flush 2>/dev/null | jq -r .query_name | sort -u | tee -a ~/recon/$1/$1-dnsgen.tmp
-	cat ~/recon/$1/$1-dnsgen.tmp | sed 's/-\.//g' | sed 's/-\.//g' | sed 's/-\-\-\-//g' | sort -u > ~/recon/$1/$1-dnsgen.txt
+	cat ~/recon/$1/$1-final.txt | dnsgen - | massdns -r ~/tools/massdns/lists/resolvers.txt -t A -o J --flush 2>/dev/null | jq -r .query_name | tee -a ~/recon/$1/$1-dnsgen.tmp
+	cat ~/recon/$1/$1-dnsgen.tmp | sed 's/-\.//g' | sed 's/-\.//g' | sed 's/-\-\-\-//g' | sed 's/.$//g' | sort -u > ~/recon/$1/$1-dnsgen.txt
 	rm ~/recon/$1/$1-dnsgen.tmp
 	sleep 3
 	dnsgens=`scanned ~/recon/$1/$1-dnsgen.txt`
